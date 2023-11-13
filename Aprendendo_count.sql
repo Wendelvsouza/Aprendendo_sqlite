96b1aef5bed154dc00955f1db55e69324565e6cc
Wendel <wendelvaccani@gmail.com>
2023-11-13 10:56:13 -0300

    Feat: estudo sobre count

--- Aprendendo_count.sql
+++ Aprendendo_count.sql
@@ -2,6 +2,6 @@ select  count(t1.product_id) as qtde_produtos,
         sum(t1.product_width_cm) / count(t1.product_id)  as media_largura,
         sum(t1.product_height_cm) / count(t1.product_id)  as media_comprimento,
         sum(t1.product_length_cm) / count(t1.product_id)  as media_altura, 
-        count(distinct t1.product_category_name) as media_produtos
+        count(distinct t1.product_category_name) as categorias_produtos
 from tb_products as t1
 
